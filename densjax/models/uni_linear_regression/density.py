ecd01d95fb93736effb7a67957c2682d2f2fc0a9
Bob Carpenter <carp@alias-i.com>
2024-11-23 11:44:21 -0500

    update linear regression to use self

--- densjax/models/uni_linear_regression/density.py
+++ densjax/models/uni_linear_regression/density.py
@@ -6,12 +6,10 @@ from ..base import BayesianModel
 from ...utils.readers import read_real, read_real_lb
 
 class UniLinearRegression(BayesianModel):
-    @classmethod
-    def num_unconstrained_parameters(cls, data):
+    def num_unconstrained_parameters(self, data):
         return 3
 
-    @classmethod
-    def constrain_parameters(cls, data, params_unc):
+    def constrain_parameters(self, data, params_unc):
         pos = 0
         alpha, pos, _ = read_real(params_unc, pos)
         beta, pos, _ = read_real(params_unc, pos)
@@ -19,24 +17,21 @@ class UniLinearRegression(BayesianModel):
         params = {'alpha': alpha, 'beta': beta, 'sigma': sigma}
         return params, log_jac
 
-    @classmethod
-    def unconstrain_parameters(cls, data, params):
+    def unconstrain_parameters(self, data, params):
         alpha = params['alpha']
         beta = params['beta']
         sigma_unc = jnp.log(params['sigma'])
         params_unconstrained = jnp.array([alpha, beta, sigma_unc])
         return params_unconstrained
 
-    @classmethod
-    def log_prior(cls, data, params):
+    def log_prior(self, data, params):
         lp_alpha = norm.logpdf(params['alpha'], loc=0.0, scale=1.0)
         lp_beta = norm.logpdf(params['beta'], loc=0.0, scale=1.0)
         # "scale" is rate of exponential distribution (bad SciPy)
         lp_sigma = expon.logpdf(params['sigma'], scale=1.0)  
         return lp_alpha + lp_beta + lp_sigma
 
-    @classmethod
-    def log_likelihood(cls, data, params):
+    def log_likelihood(self, data, params):
         x = jnp.asarray(data['x'])
         y = jnp.asarray(data['y'])
         alpha = params['alpha']
@@ -46,8 +41,7 @@ class UniLinearRegression(BayesianModel):
         log_lik = jnp.sum(norm.logpdf(y, loc=mu, scale=sigma))
         return log_lik
 
-    @classmethod
-    def generated_quantities(cls, data, params, rng):
+    def generated_quantities(self, data, params, rng):
         x_new = jnp.asarray(data['x_new'])
         alpha = params['alpha']
         beta = params['beta']
@@ -56,8 +50,7 @@ class UniLinearRegression(BayesianModel):
         y_new = mu_new + sigma * jax.random.normal(rng, shape=x_new.shape)
         return {'y_new': y_new}
         
-    @classmethod
-    def initial_draw(cls, rng, data):
+    def initial_draw(self, rng, data):
         key_alpha, key_beta, key_sigma = jax.random.split(rng, 3)
         # normal(0, 1)
         alpha = jax.random.normal(key_alpha, shape=())
